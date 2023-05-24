ab8ee8d51f3fc9458e609e1eb662436350cbf963
osuchaya <suchaya.osatis@gmail.com>
2023-05-24 18:40:43 +0100

    webpack.config elements added

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,1 +1,10 @@
 // TODO: Create a service worker that caches static assets:
+export const registerSW = () => {
+    // Check that service workers are supported
+  if ('serviceWorker' in navigator) {
+      // Use the window load event to keep the page load performant
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js');
+      });
+    }
+  };
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,8 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 module.exports = () => {
   return {
@@ -11,17 +15,33 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+    
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
+      new WorkboxPlugin.GenerateSW(),
     ],
 
     // TODO: Add the correct modules
     module: {
-
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+      ]
     }
   };
 };
